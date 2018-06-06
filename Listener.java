37ac522ab9211aedba7fa41de267ae86bfe138e8
Hayden Watson <8919564+Mrweatherman208@users.noreply.github.com>
2018-06-06 01:09:46 -0400

    Add files via upload

---
+++ Listener.java
@@ -0,0 +1,17 @@
+package gamercraftbros.maintenanceannouncer;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Listener implements org.bukkit.event.Listener {
+	
+	private Main plugin;
+	
+	public Listener (Main plugin) { this.plugin = plugin; }
+	
+	@EventHandler
+	private void onPlayerJoin(PlayerJoinEvent event) { 
+		event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("Prefix") + plugin.getConfig().getString("Maintenance ongoing message"))); 
+		}
+	}
