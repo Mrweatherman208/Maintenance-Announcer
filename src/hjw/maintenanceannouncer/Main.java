864f7c6294c8c380b9facd3acd6c4bd23d9c73bf
Hayden Watson <8919564+Mrweatherman208@users.noreply.github.com>
2021-09-28 23:37:13 -0400

    Fixed indentations

--- src/hjw/maintenanceannouncer/Main.java
+++ src/hjw/maintenanceannouncer/Main.java
@@ -11,47 +11,47 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	    	
+
 	private boolean started = false; // Var for if the maintenance to the server is active.
-	
+
 	// Plugin has been enabled.
 	@Override
 	public void onEnable() {
-   	 getLogger().info("Maintenance Announcer is enabled.");
+		getLogger().info("Maintenance Announcer is enabled.");
 		createConfig();
 		// Check if the "Maintenance" string in the config says "true".
-   	 if (getConfig().getString("Maintenance") == "true") {
+		if (getConfig().getString("Maintenance") == "true") {
 			startEvent();
-   	 }
+		}
 	}
-	
+
 	// Plugin has been disabled.
 	@Override
 	public void onDisable() { 
 		getLogger().info("Maintenance Announcer is disabled."); // Log that Maintenance Announcer has been disabled.
 	}
-	
+
 	public boolean onCommand (CommandSender sender, Command cmd, String label, String[] args) {
 		// Statements for the maintenance commands.
 		if (cmd.getName().equalsIgnoreCase("maintenance")) {
 			reloadConfig();
 			// Switch for the arugment length.
 			switch (args.length) {
-				case 0:
+			case 0:
 				if (getConfig().getString("adminOnlyMaintenance") == "true") {
 					if(sender.hasPermission("hjw.maintenanceannouncer.adminsee")) {
-		    		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance ongoing message")));
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance ongoing message")));
 					}
-		   	 }
+				}
 				if(sender.hasPermission("hjw.maintenanceannouncer.see")) {
-				   	 if (getConfig().getString("Maintenance") == "true") {
-	 			    		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString("Maintenance ongoing message")));
-				   	 } else {
-	 			    		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString("Maintenance not ongoing message")));
-				   	 }
+					if (getConfig().getString("Maintenance") == "true") {
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString("Maintenance ongoing message")));
+					} else {
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix") + getConfig().getString("Maintenance not ongoing message")));
+					}
 				}
 				return true;
-				case 1: // User just used one arguement in their command.
+			case 1: // User just used one arguement in their command.
 				// Check for "start" or "begin" being the first argument.
 				if (args[0].equalsIgnoreCase("start") || (args[0].equalsIgnoreCase("begin"))) {
 					if(sender.hasPermission("hjw.maintenanceannouncer.start")) {
@@ -75,33 +75,33 @@ public class Main extends JavaPlugin {
 					return true;
 				}
 				// Check for "admin" being the first argument.
-					if (args[0].equalsIgnoreCase("admin")) {
+				if (args[0].equalsIgnoreCase("admin")) {
+					if(sender.hasPermission("hjw.maintenanceannouncer.adminsee")) {
 						if(sender.hasPermission("hjw.maintenanceannouncer.adminsee")) {
-							if(sender.hasPermission("hjw.maintenanceannouncer.adminsee")) {
 							if (getConfig().getString("adminOnlyMaintenance") == "true") {
-					    		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance ongoing message")));
-								} else {
-						    		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance not ongoing message")));
-								}
-					   	 }
+								sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance ongoing message")));
+							} else {
+								sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance not ongoing message")));
+							}
 						}
-						return true;
+					}
+					return true;
 				}
 				// Reload Plugin
 				if (args[0].equalsIgnoreCase("reload")) {
 					if(sender.hasPermission("hjw.maintenanceannouncer.reload")) {
-							if (getConfig().getString("Allow reloads to Maintenance Announcer") == "true") {
+						if (getConfig().getString("Allow reloads to Maintenance Announcer") == "true") {
 							reloadConfig();
 							sender.sendMessage(ChatColor.GREEN + "Maintenance Announcer successfully reloaded!");
-							} else {
+						} else {
 							sender.sendMessage(ChatColor.RED + "Reloading of Maintenance Announcer was disabled in the config file!");
-							}
+						}
 					}
 					return true;
-					}
-						// Invalid Argument
-						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Invalid argument message")));
-				case 2: // User used 2 arguements in their command.
+				}
+				// Invalid Argument
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Invalid argument message")));
+			case 2: // User used 2 arguements in their command.
 				// Check for "admin" being the first argument.
 				if (args[0].equalsIgnoreCase("admin")) {
 					if (args[1].equalsIgnoreCase("start") || (args[1].equalsIgnoreCase("begin"))) {
@@ -122,67 +122,67 @@ public class Main extends JavaPlugin {
 							saveConfig();
 							reloadConfig();
 							sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Sent to all admins message")));
-							}
+						}
 						return true;
 					}
 					sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Invalid argument message")));
-					}
+				}
 				return true;
-					default: sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Two arguments are needed message")));
-				
+			default: sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Two arguments are needed message")));
+
 			}
-	}
+		}
 		return false;
-}
-	
+	}
+
 	// Maintenance was started.
 	private void startEvent() {
 		// Check if maintenance has already been started.
 		if (started == false) {
-		PluginManager pm = getServer().getPluginManager();
-		Listener listener = new Listener(this);
-		pm.registerEvents(listener, this);
-		started = true;
+			PluginManager pm = getServer().getPluginManager();
+			Listener listener = new Listener(this);
+			pm.registerEvents(listener, this);
+			started = true;
 		}
 	}
-	
+
 	private void adminBroadcastStart() {
-    	for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-    		if (player.isOp()) {
-    		player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance has started")));
-    		}
-    	}
+		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+			if (player.isOp()) {
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance has started")));
+			}
+		}
 	}
 
 	private void adminBroadcastStop() {
 		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
 			if (player.isOp()) {
-player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance has ended")));
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("Admin only prefix") + getConfig().getString("Maintenance has ended")));
 			}
 		}
 	}
 
 	private void createConfig() {
-	    try {
+		try {
 			// Check if the "MaintenanceAnnouncer" directory exists.
-	        if (getDataFolder().exists() == false) { 
+			if (getDataFolder().exists() == false) { 
 				getDataFolder().mkdirs(); // Make directory using the built-in bukkit function.
 			}
 
-	        File file = new File(getDataFolder(), "config.yml"); // Introduce the new file, spcify the directory and file name.
+			File file = new File(getDataFolder(), "config.yml"); // Introduce the new file, spcify the directory and file name.
 
 			// Check if the config file actually exists.
-	        if (file.exists() == false) {
-			   	getLogger().info("Config file not found! Creating one!"); // Log that a config file is being made.
-	            saveDefaultConfig(); // Use the save default bukkit function.
-	        } else {
-	        	getLogger().info("Config file found! Loaded!");
-	        }
-	    } catch (Exception e) {
+			if (file.exists() == false) {
+				getLogger().info("Config file not found! Creating one!"); // Log that a config file is being made.
+				saveDefaultConfig(); // Use the save default bukkit function.
+			} else {
+				getLogger().info("Config file found! Loaded!");
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		 }
+		}
 	}
-	
+
 }
 
 // You are of God, little children, and have overcome them, because He who is
